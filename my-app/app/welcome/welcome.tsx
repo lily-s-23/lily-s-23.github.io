bce7ac1a3fecfe056fec72511f0699ae60b027af
Lily Smales <lily.smales@shopify.com>
2024-12-29 16:09:15 -0500

    setup react router

---
+++ my-app/app/welcome/welcome.tsx
@@ -0,0 +1,14 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div>
+        <h1>
+          Hello!!
+        </h1>
+      </div>
+    </main>
+  );
+}

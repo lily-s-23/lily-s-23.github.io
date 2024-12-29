0f8e387c44172113d7407eae5f16be4cecd60230
Lily Smales <lily.smales@shopify.com>
2024-12-29 16:22:50 -0500

    cleaned up template

--- my-app/app/welcome/welcome.tsx
+++ my-app/app/welcome/welcome.tsx
@@ -1,13 +1,13 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
-
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div>
         <h1>
-          Hello!!
+          Hello, my name is Lily Smales,
         </h1>
+        <p>
+          I'm currently a Dev Degree Intern at Shopify and a Computer Science Student at Carleton University.
+        </p>
       </div>
     </main>
   );
